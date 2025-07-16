b124b180a95377ac42430e8112ac37d8e37dd2e4
nketomaa <nooa.ketomaa@helsinki.fi>
2025-07-16 12:01:37 +0300

    Copied Harris.cpp from Hongyang's slab branch

--- projects/Harris/Harris.cpp
+++ projects/Harris/Harris.cpp
@@ -20,126 +20,203 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
+#include <cmath>
 #include <cstdlib>
 #include <iostream>
-#include <cmath>
 
-#include "../../common.h"
-#include "../../readparameters.h"
 #include "../../backgroundfield/backgroundfield.h"
+#include "../../common.h"
 #include "../../object_wrapper.h"
+#include "../../readparameters.h"
 
 #include "Harris.h"
 
-using namespace std;
 using namespace spatial_cell;
 
 namespace projects {
-   Harris::Harris(): TriAxisSearch() { }
-   Harris::~Harris() { }
-   
-   bool Harris::initialize(void) {return Project::initialize();}
-   
-   void Harris::addParameters(){
-      typedef Readparameters RP;
-      RP::add("Harris.Scale_size", "Harris sheet scale size (m)", 150000.0);
-      RP::add("Harris.BX0", "Magnetic field at infinity (T)", 8.33061003094e-8);
-      RP::add("Harris.BY0", "Magnetic field at infinity (T)", 8.33061003094e-8);
-      RP::add("Harris.BZ0", "Magnetic field at infinity (T)", 8.33061003094e-8);
+Harris::Harris() : TriAxisSearch() {}
+Harris::~Harris() {}
 
-      // Per-population parameters
-      for(uint i=0; i< getObjectWrapper().particleSpecies.size(); i++) {
-         const std::string& pop = getObjectWrapper().particleSpecies[i].name;
+bool Harris::initialize(void) { return Project::initialize(); }
 
-         RP::add(pop + "_Harris.Temperature", "Temperature (K)", 2.0e6);
-         RP::add(pop + "_Harris.rho", "Number density at infinity (m^-3)", 1.0e7);
+void Harris::addParameters() {
+   typedef Readparameters RP;
+   RP::add("Harris.currentSheetType", "Type of the current sheet initialization: 1. single layer, 2. double layer", 1);
+   RP::add("Harris.scale", "Harris sheet scale size (m)", 150000.0);
+   RP::add("Harris.Bx0", "Reference Magnetic field (T)", 8.33061003094e-8);
+   RP::add("Harris.By0", "Reference Magnetic field (T)", 8.33061003094e-8);
+   RP::add("Harris.Bz0", "Reference Magnetic field (T)", 8.33061003094e-8);
+   RP::add("Harris.Psi0", "Perturbation of the magnetic flux (T*m)", 0.0);
+
+   // Per-population parameters
+   for (uint i = 0; i < getObjectWrapper().particleSpecies.size(); i++) {
+      const std::string& pop = getObjectWrapper().particleSpecies[i].name;
+
+      RP::add(pop + "_Harris.n", "Reference number density (m^-3)", 1.0e6);
+      RP::add(pop + "_Harris.T", "Species temperature", 1.0);
+      RP::add(pop + "_Harris.nSpaceSamples", "Number of sampling points per spatial dimension.", 2);
+      RP::add(pop + "_Harris.nVelocitySamples", "Number of sampling points per velocity dimension.", 2);
+   }
+}
+
+void Harris::getParameters() {
+   Project::getParameters();
+   typedef Readparameters RP;
+   RP::get("Harris.currentSheetType", currentSheetType);
+   RP::get("Harris.scale", lambda);
+   RP::get("Harris.Bx0", Bx0);
+   RP::get("Harris.By0", By0);
+   RP::get("Harris.Bz0", Bz0);
+   RP::get("Harris.Psi0", Psi0);
+
+   if (currentSheetType != 1 && currentSheetType != 2) {
+      int myRank;
+      MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
+      if (myRank == MASTER_RANK) {
+         std::cerr << "unknown initialization type: " << currentSheetType << std::endl;
+         MPI_Abort(MPI_COMM_WORLD, 1);
       }
    }
-   
-   void Harris::getParameters(){
-      Project::getParameters();
-      typedef Readparameters RP;
-      RP::get("Harris.Scale_size", this->SCA_LAMBDA);
-      RP::get("Harris.BX0", this->BX0);
-      RP::get("Harris.BY0", this->BY0);
-      RP::get("Harris.BZ0", this->BZ0);
 
+   // Per-population parameters
+   for (uint i = 0; i < getObjectWrapper().particleSpecies.size(); i++) {
+      const std::string& pop = getObjectWrapper().particleSpecies[i].name;
+      HarrisSpeciesParameters sP;
 
-      // Per-population parameters
-      for(uint i=0; i< getObjectWrapper().particleSpecies.size(); i++) {
-         const std::string& pop = getObjectWrapper().particleSpecies[i].name;
-         HarrisSpeciesParameters sP;
+      RP::get(pop + "_Harris.n", sP.n);
+      RP::get(pop + "_Harris.T", sP.T);
+      RP::get(pop + "_Harris.nSpaceSamples", sP.nSpaceSamples);
+      RP::get(pop + "_Harris.nVelocitySamples", sP.nVelocitySamples);
 
-         RP::get(pop + "_Harris.Temperature", sP.TEMPERATURE);
-         RP::get(pop + "_Harris.rho", sP.DENSITY);
-
-         speciesParams.push_back(sP);
-      }
+      speciesParams.push_back(sP);
    }
-   
-   Real Harris::getDistribValue(
-      creal& x,creal& y, creal& z,
-      creal& vx, creal& vy, creal& vz,
-      creal& dvx, creal& dvy, creal& dvz,
-      const uint popID
-   ) const {
+}
 
-      const HarrisSpeciesParameters& sP = speciesParams[popID];
-      Real mass = getObjectWrapper().particleSpecies[popID].mass;
+Real Harris::getMaxwellian(creal& x, creal& y, creal& z, creal& vx, creal& vy, creal& vz, creal& dvx, creal& dvy,
+                           creal& dvz, const uint popID) const {
+   const HarrisSpeciesParameters& sP = speciesParams[popID];
+   Real mass = getObjectWrapper().particleSpecies[popID].mass;
+   auto KB = physicalconstants::K_B;
 
-      return sP.DENSITY * pow(mass / (2.0 * M_PI * physicalconstants::K_B * sP.TEMPERATURE), 1.5) * (
-         5.0 / pow(cosh(x / (this->SCA_LAMBDA)), 2.0) * exp(- mass * (pow(vx, 2.0) + pow(vy, 2.0) + pow(vz, 2.0)) / (2.0 * physicalconstants::K_B * sP.TEMPERATURE))
-         +
-         exp(- mass * (pow(vx, 2.0) + pow(vy, 2.0) + pow(vz, 2.0)) / (2.0 * physicalconstants::K_B * sP.TEMPERATURE)));
-   }
-   
-   Real Harris::calcPhaseSpaceDensity(
-      creal& x,creal& y,creal& z,
-      creal& dx,creal& dy,creal& dz,
-      creal& vx,creal& vy,creal& vz,
-      creal& dvx,creal& dvy,creal& dvz,const uint popID
-   ) const {
-      return getDistribValue(x+0.5*dx, y+0.5*dy, z+0.5*dz, vx+0.5*dvx, vy+0.5*dvy, vz+0.5*dvz, dvx, dvy, dvz, popID);      
-   }
-   
-   void Harris::calcCellParameters(spatial_cell::SpatialCell* cell,creal& t) { }
-   
-   vector<std::array<Real, 3>> Harris::getV0(
-      creal x,
-      creal y,
-      creal z,
-      const uint popID
-   ) const {
-      vector<std::array<Real, 3>> V0;
-      std::array<Real, 3> v = {{0.0, 0.0, 0.0 }};
-      V0.push_back(v);
-      return V0;
+   Real f, n;
+
+   if (currentSheetType == 1) {
+      Real sech2 = sqr(1.0 / std::cosh(z / lambda));
+      n = sP.n * sech2 + 0.2 * sP.n;
+   } else if (currentSheetType == 2) {
+      const Real Lx = P::xmax - P::xmin;
+      const Real Lz = P::zmax - P::zmin;
+
+      n = sP.n * (sqr(1.0 / std::cosh((z - 0.25 * Lz) / lambda)) + sqr(1.0 / std::cosh((z + 0.25 * Lz) / lambda))) +
+          0.2 * sP.n;
    }
 
-   void Harris::setProjectBField(
-      FsGrid< std::array<Real, fsgrids::bfield::N_BFIELD>, FS_STENCIL_WIDTH> & perBGrid,
-      FsGrid< std::array<Real, fsgrids::bgbfield::N_BGB>, FS_STENCIL_WIDTH> & BgBGrid,
-      FsGrid< fsgrids::technical, FS_STENCIL_WIDTH> & technicalGrid
-   ) {
-      setBackgroundFieldToZero(BgBGrid);
-      
-      if(!P::isRestart) {
-         auto localSize = perBGrid.getLocalSize().data();
-         
-         #pragma omp parallel for collapse(3)
-         for (FsGridTools::FsIndex_t x = 0; x < localSize[0]; ++x) {
-            for (FsGridTools::FsIndex_t y = 0; y < localSize[1]; ++y) {
-               for (FsGridTools::FsIndex_t z = 0; z < localSize[2]; ++z) {
-                  const std::array<Real, 3> xyz = perBGrid.getPhysicalCoords(x, y, z);
-                  std::array<Real, fsgrids::bfield::N_BFIELD>* cell = perBGrid.get(x, y, z);
-                  
-                  cell->at(fsgrids::bfield::PERBX) = this->BX0 * tanh((xyz[1] + 0.5 * perBGrid.DY) / this->SCA_LAMBDA);
-                  cell->at(fsgrids::bfield::PERBY) = this->BY0 * tanh((xyz[2] + 0.5 * perBGrid.DZ) / this->SCA_LAMBDA);
-                  cell->at(fsgrids::bfield::PERBZ) = this->BZ0 * tanh((xyz[0] + 0.5 * perBGrid.DX) / this->SCA_LAMBDA);
+   f = n * std::pow(mass / (2.0 * M_PI * KB * sP.T), 1.5) *
+       exp(-mass * (sqr(vx) + sqr(vy) + sqr(vz)) / (2.0 * KB * sP.T));
+
+   return f;
+}
+
+Real Harris::getBiMaxwellian(const uint popID, creal rho, creal Tpar, creal Tperp, creal vpar, creal vperp) const {
+   const Real MASS = getObjectWrapper().particleSpecies[popID].mass;
+   Real c1 = MASS / (2.0 * physicalconstants::K_B);
+   Real c2 = c1 / M_PI;
+   Real f = rho / (sqrt(Tpar) * Tperp) * sqrt(c2) * c2 * exp(-c1 * (vperp * vperp) / Tperp - c1 * vpar * vpar / Tpar);
+   return f;
+}
+
+Real Harris::calcPhaseSpaceDensity(creal& x, creal& y, creal& z, creal& dx, creal& dy, creal& dz, creal& vx, creal& vy,
+                                   creal& vz, creal& dvx, creal& dvy, creal& dvz, const uint popID) const {
+   const HarrisSpeciesParameters& sP = speciesParams[popID];
+
+   if ((sP.nSpaceSamples > 1) && (sP.nVelocitySamples > 1)) {
+      creal d_x = dx / (sP.nSpaceSamples - 1);
+      creal d_y = dy / (sP.nSpaceSamples - 1);
+      creal d_z = dz / (sP.nSpaceSamples - 1);
+      creal d_vx = dvx / (sP.nVelocitySamples - 1);
+      creal d_vy = dvy / (sP.nVelocitySamples - 1);
+      creal d_vz = dvz / (sP.nVelocitySamples - 1);
+
+      Real avg = 0.0;
+
+      for (uint i = 0; i < sP.nSpaceSamples; ++i)
+         for (uint j = 0; j < sP.nSpaceSamples; ++j)
+            for (uint k = 0; k < sP.nSpaceSamples; ++k)
+               for (uint vi = 0; vi < sP.nVelocitySamples; ++vi)
+                  for (uint vj = 0; vj < sP.nVelocitySamples; ++vj)
+                     for (uint vk = 0; vk < sP.nVelocitySamples; ++vk) {
+                        avg += getMaxwellian(x + i * d_x, y + j * d_y, z + k * d_z, vx + vi * d_vx, vy + vj * d_vy,
+                                             vz + vk * d_vz, dvx, dvy, dvz, popID);
+                     }
+      return avg / (sP.nSpaceSamples * sP.nSpaceSamples * sP.nSpaceSamples) /
+             (sP.nVelocitySamples * sP.nVelocitySamples * sP.nVelocitySamples);
+   } else {
+      return getMaxwellian(x + 0.5 * dx, y + 0.5 * dy, z + 0.5 * dz, vx + 0.5 * dvx, vy + 0.5 * dvy, vz + 0.5 * dvz,
+                           dvx, dvy, dvz, popID);
+   }
+}
+
+void Harris::calcCellParameters(spatial_cell::SpatialCell* cell, creal& t) {}
+
+std::vector<std::array<Real, 3>> Harris::getV0(creal x, creal y, creal z, const uint popID) const {
+   std::vector<std::array<Real, 3>> V0;
+   std::array<Real, 3> v = {{0.0, 0.0, 0.0}};
+   V0.push_back(v);
+   return V0;
+}
+
+void Harris::setProjectBField(FsGrid<std::array<Real, fsgrids::bfield::N_BFIELD>, FS_STENCIL_WIDTH>& perBGrid,
+                              FsGrid<std::array<Real, fsgrids::bgbfield::N_BGB>, FS_STENCIL_WIDTH>& BgBGrid,
+                              FsGrid<fsgrids::technical, FS_STENCIL_WIDTH>& technicalGrid) {
+   setBackgroundFieldToZero(BgBGrid);
+
+   const Real Lx = P::xmax - P::xmin;
+   const Real Lz = P::zmax - P::zmin;
+
+   if (!P::isRestart) {
+      auto localSize = perBGrid.getLocalSize().data();
+
+      if (currentSheetType == 1) {
+#pragma omp parallel for collapse(3)
+         for (int i = 0; i < localSize[0]; ++i) {
+            for (int j = 0; j < localSize[1]; ++j) {
+               for (int k = 0; k < localSize[2]; ++k) {
+                  const std::array<Real, 3> x = perBGrid.getPhysicalCoords(i, j, k);
+                  std::array<Real, fsgrids::bfield::N_BFIELD>* cell = perBGrid.get(i, j, k);
+
+                  cell->at(fsgrids::bfield::PERBX) = Bx0 * tanh((x[2] + 0.5 * perBGrid.DZ) / lambda) +
+                                                     Psi0 * (-M_PI / Lz) *
+                                                         cos(2 * M_PI * (x[0] + 0.5 * perBGrid.DX) / Lx) *
+                                                         sin(M_PI * (x[2] + 0.5 * perBGrid.DZ) / Lz);
+                  cell->at(fsgrids::bfield::PERBY) = 0.0;
+                  cell->at(fsgrids::bfield::PERBZ) = Psi0 * (-2 * M_PI / Lx) *
+                                                     sin(2 * M_PI * (x[0] + 0.5 * perBGrid.DX) / Lx) *
+                                                     cos(M_PI * (x[2] + 0.5 * perBGrid.DZ) / Lz);
+               }
+            }
+         }
+      } else if (currentSheetType == 2) {
+#pragma omp parallel for collapse(3)
+         for (int i = 0; i < localSize[0]; ++i) {
+            for (int j = 0; j < localSize[1]; ++j) {
+               for (int k = 0; k < localSize[2]; ++k) {
+                  const std::array<Real, 3> x = perBGrid.getPhysicalCoords(i, j, k);
+                  std::array<Real, fsgrids::bfield::N_BFIELD>* cell = perBGrid.get(i, j, k);
+
+                  cell->at(fsgrids::bfield::PERBX) =
+                      Bx0 * (tanh((x[2] + 0.5 * perBGrid.DZ - 0.25 * Lz) / lambda) -
+                             tanh((x[2] + 0.5 * perBGrid.DZ + 0.25 * Lz) / lambda) + 1.0) +
+                      Psi0 * (-M_PI / Lz) * cos(2 * M_PI * (x[0] + 0.5 * perBGrid.DX) / Lx) *
+                          sin(2 * M_PI * (x[2] + 0.5 * perBGrid.DZ) / Lz - 0.25);
+                  cell->at(fsgrids::bfield::PERBY) = 0.0;
+                  cell->at(fsgrids::bfield::PERBZ) = Psi0 * (-2 * M_PI / Lx) *
+                                                     sin(2 * M_PI * (x[0] + 0.5 * perBGrid.DX) / Lx) *
+                                                     cos(2 * M_PI * (x[2] + 0.5 * perBGrid.DZ) / Lz);
                }
             }
          }
       }
    }
+}
 
 } // namespace projects
