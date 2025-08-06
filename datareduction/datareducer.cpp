2fab68f87e70c7c3062c13238db2e07330e6a616
nketomaa <nooa.ketomaa@helsinki.fi>
2025-08-06 16:25:32 +0300

    Added a relative entropy datareducer

--- datareduction/datareducer.cpp
+++ datareduction/datareducer.cpp
@@ -552,6 +552,19 @@ void initializeDataReducers(DataReducer * outputReducer, DataReducer * diagnosti
             continue;
          }
       }
+      if (P::systemWriteAllDROs || lowercase == "populations_relative_entropy" || lowercase == "populations_vg_relative_entropy") {
+         // Relative entropy of a population
+         for (unsigned int i = 0; i < getObjectWrapper().particleSpecies.size(); i++) {
+            species::Species& species = getObjectWrapper().particleSpecies[i];
+            const std::string& pop = species.name;
+            outputReducer->addOperator(new DRO::VariableRelativeEntropy(i));
+            outputReducer->addMetadata(outputReducer->size() - 1, "J/K", "$\\mathrm{J}\\,\\mathrm{K}^{-1}$",
+                                       "$s_{\\mathrm{rel," + pop + "}}$", "1.0");
+         }
+         if(!P::systemWriteAllDROs) {
+            continue;
+         }
+      }
       if(P::systemWriteAllDROs || lowercase == "maxfieldsdt" || lowercase == "fg_maxfieldsdt" || lowercase == "fg_maxdt_fieldsolver") {
          // Maximum timestep constraint as calculated by the fieldsolver
          outputReducer->addOperator(new DRO::DataReductionOperatorFsGrid("fg_maxdt_fieldsolver",[](
